07db63cdb4dc5f14b6ba38710a3cadb2c4210236
Dov Kruger <Dov.Kruger@gmail.com>
2024-10-08 17:37:18 -0400

    memory timings

--- sessions/reviewtest1/memorytimings.cpp
+++ sessions/reviewtest1/memorytimings.cpp
@@ -69,45 +69,90 @@ double j(volatile double x[], int stride,int n) {
     return sum;
 }
 
+void k(volatile double x[], int stride,int n) {
+    const int size = 1024;
+    for (int i = 0; i < n; i+=size) {
+        for (int j = 0; j < size; j++)
+            x[j]++;
+    }
+}
+
 int main() {
     const int n = 100'000'000;
+    const int num_trials = 100;
     double* x = new double[n];
 
-    double sum;
+    double sum = 0;
     auto start = chrono::high_resolution_clock::now();
-    sum  = a(x, n);
+    for (int trial = 0; trial < num_trials; trial++) {
+        sum  = a(x, n);
+    }
     auto end = chrono::high_resolution_clock::now();
-    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6 << "sum = " << sum << endl;
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
 
     start = chrono::high_resolution_clock::now();
-    sum  = b(x, n);
+    for (int trial = 0; trial < num_trials; trial++) {
+        sum  = b(x, n);
+    }
     end = chrono::high_resolution_clock::now();
-    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6 << "sum = " << sum << endl;
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
 
     start = chrono::high_resolution_clock::now();
-    sum  = c(x, n);
+    for (int trial = 0; trial < num_trials; trial++) {
+        sum  = c(x, n);
+    }
     end = chrono::high_resolution_clock::now();
-    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6 << "sum = " << sum << endl;
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
 
     start = chrono::high_resolution_clock::now();
-    sum  = d(x, n);
+    for (int trial = 0; trial < 10; trial++) {
+        sum  = d(x, n);
+    }
     end = chrono::high_resolution_clock::now();
-    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6 << "sum = " << sum << endl;
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
 
     start = chrono::high_resolution_clock::now();
-    e(x, n);
+    for (int trial = 0; trial < num_trials; trial++) {
+        e(x, n);
+    }
     end = chrono::high_resolution_clock::now();
-    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6 << "sum = " << sum << endl;
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
+
+    for (int stride = 1; stride <= 1024; stride*=2) {
+        start = chrono::high_resolution_clock::now();
+        for (int trial = 0; trial < num_trials; trial++) {
+            f(x, stride, n);
+        }
+        end = chrono::high_resolution_clock::now();
+        cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
+    }
+    start = chrono::high_resolution_clock::now();
+    for (int trial = 0; trial < num_trials; trial++) {
+        sum  = g(x, 16, n);
+    }
+    end = chrono::high_resolution_clock::now();
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
 
     start = chrono::high_resolution_clock::now();
-    sum  = f(x, n);
+    for (int trial = 0; trial < num_trials; trial++) {
+        sum  = h(x, 16, n);
+    }
     end = chrono::high_resolution_clock::now();
-    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6 << "sum = " << sum << endl;
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
 
     start = chrono::high_resolution_clock::now();
-    sum  = g(x, 16, n);
+    for (int trial = 0; trial < 10; trial++) {
+        sum  = j(x, 16, n);
+    }
     end = chrono::high_resolution_clock::now();
-    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6 << "sum = " << sum << endl;
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/num_trials << "sum = " << sum << endl;
 
+    start = chrono::high_resolution_clock::now();
+    for (int trial = 0; trial < num_trials; trial++) {
+        k(x, 16, n);
+    }
+    end = chrono::high_resolution_clock::now();
+    cout << chrono::duration_cast<chrono::microseconds>(end - start).count()*1e-6/10 << "sum = " << sum << endl;
+    
     return 0;
 }
