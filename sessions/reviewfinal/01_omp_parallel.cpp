867cde1d2ff72f83576fac3f07c1eb72e08389b7
Dov Kruger <Dov.Kruger@gmail.com>
2024-12-10 19:09:40 -0500

    final review

---
+++ sessions/reviewfinal/01_omp_parallel.cpp
@@ -0,0 +1,11 @@
+#include <omp.h>
+#include <stdio.h>
+
+int main() {
+    #pragma omp parallel
+    {
+        int thread_id = omp_get_thread_num();
+        printf("Hello from thread %d\n", thread_id);
+    }
+    return 0;
+}
