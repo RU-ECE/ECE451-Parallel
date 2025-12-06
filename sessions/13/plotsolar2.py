5b0647bb193de654669902a491f543f1350dab9e
Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>
2025-12-05 20:45:12 -0500

    Reformat Python files
    Signed-off-by: Chinay-Domitrix <55807470+Chinay-Domitrix@users.noreply.github.com>

--- sessions/13/plotsolar2.py
+++ sessions/13/plotsolar2.py
@@ -1,11 +1,13 @@
 import sys
-import matplotlib.pyplot as plt
+
+from matplotlib.pyplot import plot, show, subplots, title, pause
 
 xlim = 7e11
 ylim = 7e11
 
+
 def plot_bodies(filename):
-    fig, ax = plt.subplots()
+    fig, ax = subplots()
     ax.set_xlim(-xlim, xlim)
     ax.set_ylim(-ylim, ylim)
 
@@ -16,7 +18,7 @@ def plot_bodies(filename):
             bodies = []
             parts = line.split()
             for i in range(0, len(parts), 4):
-                name, x, y, z = parts[i:i+4]
+                name, x, y, z = parts[i:i + 4]
                 x, y = float(x), float(y)
                 bodies.append((x, y))
             ax.clear()
@@ -24,17 +26,19 @@ def plot_bodies(filename):
             ax.set_ylim(-ylim, ylim)
             sizes = [5 if i == 0 else 1 for i in range(len(bodies))]
             ax.scatter([x for x, y in bodies], [y for x, y in bodies], s=sizes)
-            plt.pause(1)  # Pause for 1 second to achieve 1 fps
-    plt.show()
+            pause(1)  # Pause for 1 second to achieve 1 fps
+    show()
+
 
 def simple_plot():
-    plt.plot([0, 1, 2], [0, 1, 4])
-    plt.title("Simple Plot")
-    plt.show()
+    plot([0, 1, 2], [0, 1, 4])
+    title("Simple Plot")
+    show()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python plotsolar2.py <filename>")
         sys.exit(1)
-#    simple_plot()
+    # simple_plot()
     plot_bodies(sys.argv[1])
