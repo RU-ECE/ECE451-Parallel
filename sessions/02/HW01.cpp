44402ea2ed84a37e14009ec2f84682df6113587c
Dov Kruger <Dov.Kruger@gmail.com>
2026-09-10 20:48:55 -0400

    created 2 homeworks, brute force prime and eratosthenes

---
+++ sessions/02/HW01.cpp
@@ -0,0 +1,15 @@
+/*
+
+HW01:
+Author: Firstname Lastname
+Cite:
+
+1. Run the code single threaded
+2. benchmark the code multi threaded writing to *pcount every time (Slower)
+3. benchmark the code multi threaded with a pool of 2 threads (4 optional)
+4. Now you write partitioning. Divide not into 2 but into chunks
+
+Put the table of results in the comment.
+
+
+*/
